4260d7848c8dfc294b5ca78e04f5b040d994703e
arshdeepsynergy <93897206+arshdeepsynergy@users.noreply.github.com>
2025-02-28 10:27:02 +0530

    command added to generate post-install.sh file

--- common-scripts/post-install.sh
+++ common-scripts/post-install.sh
@@ -1,4 +1,5 @@
 echo "Running post-install script..."
+mkdir -p /app/generated-proto
 npx grpc_tools_node_protoc --version
 npx grpc_tools_node_protoc --js_out=import_style=commonjs,binary:/app/generated-proto --grpc_out=grpc_js:/app/generated-proto --ts_out=/app/generated-proto -I /app/proto /app/proto/*.proto
 node /app/common-scripts/update-file-import.js
